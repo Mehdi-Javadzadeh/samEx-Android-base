22554f2060ce60fa631213e89772cea06ebe4d39
Mehdi-Javadzadeh <75339855+Mehdi-Javadzadeh@users.noreply.github.com>
2022-06-16 13:52:10 +0430

    commited by Mehdi

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>hi SamEx</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100,
+    backgroundColor: '#fff',
+  },
+})
+
+export default App
